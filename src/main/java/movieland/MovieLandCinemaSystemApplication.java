02b76994ab8570dc92d51e4beae5dd383183354d
Ignatovw99 <lyuboslavw@gmail.com>
2019-10-17 23:16:15 +0300

    Initial Commit

---
+++ src/main/java/movieland/MovieLandCinemaSystemApplication.java
@@ -0,0 +1,13 @@
+package movieland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLandCinemaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieLandCinemaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/movieland/MovieLandCinemaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package movieland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLandCinemaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
