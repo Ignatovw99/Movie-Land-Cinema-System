160986bd428cc49f02e4ce6f0da932bfca995971
Ignatovw99 <lyuboslavw@gmail.com>
2020-07-12 16:18:02 +0300

    Base projection seat generation created(front-end)

---
+++ src/main/java/movieland/domain/models/view/programme/ProgrammeViewModel.java
@@ -0,0 +1,49 @@
+package movieland.domain.models.view.programme;
+
+import java.time.LocalDate;
+
+public class ProgrammeViewModel {
+
+    private String cinemaId;
+
+    private String cinemaName;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    public ProgrammeViewModel() {
+    }
+
+    public String getCinemaId() {
+        return cinemaId;
+    }
+
+    public void setCinemaId(String cinemaId) {
+        this.cinemaId = cinemaId;
+    }
+
+    public String getCinemaName() {
+        return cinemaName;
+    }
+
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
