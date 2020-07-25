82a9227b27b171eee2dc1368cce1230c45df3479
Ignatovw99 <lyuboslavw@gmail.com>
2020-07-25 20:47:51 +0300

    Create programme for cinemas with inactive ones and seed DB with authorities on launch

--- src/main/java/movieland/MovieLandCinemaSystemApplication.java
+++ src/main/java/movieland/MovieLandCinemaSystemApplication.java
@@ -1,13 +1,32 @@
 package movieland;
 
+import movieland.services.interfaces.ProgrammesService;
+import movieland.services.interfaces.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieLandCinemaSystemApplication {
+public class MovieLandCinemaSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MovieLandCinemaSystemApplication.class, args);
     }
 
+    private final ProgrammesService programmesService;
+
+    private final UsersService usersService;
+
+    @Autowired
+    public MovieLandCinemaSystemApplication(ProgrammesService programmesService, UsersService usersService) {
+        this.programmesService = programmesService;
+        this.usersService = usersService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        programmesService.createAnActiveProgrammeForAllCinemasWithInactiveOnes();
+        usersService.seedDatabaseWithAuthorities();
+    }
 }
